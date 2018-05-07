9384e2a04f338fc06d14ff1f07ccecb4aee72c3c
Quique <quiquealvarezmonedero@gmail.com>
2018-05-07 16:45:08 +0200

    Add giphy trending call

--- app/src/main/java/com/theleftbit/gifwallet/presentation/GifListFragment.kt
+++ app/src/main/java/com/theleftbit/gifwallet/presentation/GifListFragment.kt
@@ -6,8 +6,11 @@ import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.giphy.sdk.core.models.enums.MediaType
+import com.giphy.sdk.core.network.api.GPHApiClient
 import com.theleftbit.gifwallet.R
 import kotlinx.android.synthetic.main.fragment_gif_list.*
+import kotlin.collections.ArrayList
 
 class GifListFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
@@ -18,23 +21,16 @@ class GifListFragment: Fragment() {
         var adapter = GifListAdapter()
         fragmentGifListRecyclerView.layoutManager = GridLayoutManager(context, 2)
         fragmentGifListRecyclerView.adapter = adapter
-        adapter.items = mutableListOf<String>().apply {
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
-            add("https://media.giphy.com/media/xTiTnqUxyWbsAXq7Ju/giphy.gif")
+        val client = GPHApiClient("API-KEY")
+        client.trending(MediaType.gif, null, null, null) { result, _ ->
+            if (result != null && result.data != null) {
+                val listOfGifs = ArrayList<String>()
+                result.data.forEach {
+                    listOfGifs.add(it.images.original.gifUrl)
+                }
+                adapter.items = listOfGifs
+                adapter.notifyDataSetChanged()
+            }
         }
-        adapter.notifyDataSetChanged()
     }
 }
