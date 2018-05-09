520cbd1607bbad1f1cab0014aadfb75ea3b89781
Quique <quiquealvarezmonedero@gmail.com>
2018-05-09 17:20:41 +0200

    Update onCreateView method

--- app/src/main/java/com/theleftbit/gifwallet/presentation/GifListFragment.kt
+++ app/src/main/java/com/theleftbit/gifwallet/presentation/GifListFragment.kt
@@ -10,8 +10,8 @@ import com.theleftbit.gifwallet.R
 import kotlinx.android.synthetic.main.fragment_gif_list.*
 
 class GifListFragment: Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-        LayoutInflater.from(context).inflate(R.layout.fragment_gif_list, container, false)!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        LayoutInflater.from(context).inflate(R.layout.fragment_gif_list, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
